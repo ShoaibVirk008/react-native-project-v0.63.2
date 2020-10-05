e7d096a499476e7417d5de08c70f449a769f2078
shoaibvirk008 <shoaibvirk008@gmail.com>
2020-10-05 15:42:43 +0500

    name changes

--- android/app/src/main/java/com/rn_structure_63/MainActivity.java
+++ android/app/src/main/java/com/rn_structure_63/MainActivity.java
@@ -1,4 +1,4 @@
-package com.walkfriend;
+package com.rn_structure_63;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "WalkFriend";
+    return "RN_Structure_63";
   }
 }
--- ios/RN_Structure_63Tests/RN_Structure_63Tests.m
+++ ios/RN_Structure_63Tests/RN_Structure_63Tests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface WalkFriendTests : XCTestCase
+@interface RN_Structure_63Tests : XCTestCase
 
 @end
 
-@implementation WalkFriendTests
+@implementation RN_Structure_63Tests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
